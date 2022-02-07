65772615ba0c459347ef278a1403634b1da6f5f6
LSH <oute1001@naver.com>
2022-02-07 21:25:01 +0900

    2nd Test Version
    add Box Moving

--- Prototype/Assets/Scripts(C#)/MoonController.cs
+++ Prototype/Assets/Scripts(C#)/MoonController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MoonController : MonoBehaviour
 {
+    public GameObject Girl;
+
+    GirlController girlControl;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class MoonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        girlControl = Girl.GetComponent<GirlController>();
+        transform.position = new Vector2(transform.position.x - girlControl.direction * girlControl.speed * 0.00015f, transform.position.y);
     }
 }
